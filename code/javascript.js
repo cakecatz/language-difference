3f5fdb8a4b290f09d2a317f19a319ec775eabb51
Ryo Narita <info@codedoc.net>
2015-02-27 20:18:49 +0900

    update document

--- code/javascript.js
+++ code/javascript.js
@@ -1,2 +1,27 @@
-var variable = 1;
-var number = 1.0;
+/*
+  型付け   : 弱い動的型付け
+  パラダイム: スクリプティング、オブジェクト指向、命令型、関数型
+*/
+
+// Primitive
+
+var string     = "String"
+var number     = 12345
+var bool       = true
+var _null      = null
+var _undefined = undefined
+
+// New in ECMAScript 6
+var sym        = Symbol("foo");
+
+// Function
+
+function func(args) {
+
+}
+
+var func = function(args) {
+
+}
+
+// Note: Two above functions is not equal.
